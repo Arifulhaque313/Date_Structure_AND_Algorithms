f0ea375db8eaa46d76d8a278186fffb877d61c29
arifulhaque313 <asajib7654@gmail.com>
2023-08-01 02:28:49 +0600

    add: Binary Sort and Selection Sort

---
+++ SelectionSort.py
@@ -0,0 +1,17 @@
+def binarySearch(a,n):
+        
+        for step in range(n):
+                min_index = step
+                for i in range(step+1,n):
+                        if(a[i] < a[min_index]):
+                                min_index = i
+
+
+                (a[step],a[min_index]) = (a[min_index],a[step])
+                    
+
+                     
+a = [18,13,8,10,6,16,2,19,20]
+binarySearch(a,9)
+
+print (a)
